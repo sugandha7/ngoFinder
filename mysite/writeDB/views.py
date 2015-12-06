44fe6163a325e79f398fbcaa89ed2b010a9623af
Sugandha Goel <sugandhagoel7@gmail.com>
2015-12-06 19:44:10 +0530

    Added view for first 100 entries.

--- mysite/writeDB/views.py
+++ mysite/writeDB/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from .models import ngo
+from django.db.models import Avg
+import simplejson
 
 
 def index(request):
-    objects = ngo.objects.order_by('address')[:100]
-    context = {'address': objects}
-    #return render(request, 'writeDB/index.html', context)
-    return render(request, 'writeDB/markers.html')
+    objects = ngo.objects.order_by('id')[:100]
+    location = []
+    for entry in objects:
+    	loc = []
+    	loc.append(entry.name)
+    	loc.append(entry.latitude)
+    	loc.append(entry.longitude)
+    	loc.append(entry.id)
+    	location.append(loc)
+    avg_lat = objects.aggregate(Avg('latitude'))
+    avg_lng = objects.aggregate(Avg('longitude'))
+    json_loc = simplejson.dumps(location)
+    context = {'location': json_loc, 'avg_lat': avg_lat['latitude__avg'], 'avg_lng': avg_lng['longitude__avg']}
+    return render(request, 'writeDB/markers.html', context)
 
